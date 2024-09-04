bb5ce24a31752844bfdd0e4cb4aa58f6127e111a
Limas <125028001+FlaviaLimas@users.noreply.github.com>
2024-09-04 07:57:17 -0300

    Delete aleatorio.js

--- aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Fernanda", "Juliana", "Maria Fernanda", "Marcos", "Aline", "Gustavo", "Gabriel"];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
